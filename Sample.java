81b52cc1eeca4cda3882a7b1982fcdafbf895987
MchaitanyaAshok <chaitanyamosuganti@kluniversity.in>
2025-03-19 17:02:46 +0530

    completed

--- Sample.java
+++ Sample.java
@@ -2,5 +2,6 @@ class Sample{
 public static void main(String args[])
 {
 System.out.println("Welcome to Jenkins");
-}
+System.out.println("Connecting Jenkins from Github");
+System.out.println("Hi, Welcome S-12 Students for CloudDevops Practical");
 }
